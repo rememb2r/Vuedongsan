c94cfd9ad2798797912db33af2415b9907983130
Justin Z <>
2022-04-18 12:51:34 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+	 <h1>Hello Vue</h1>
+ </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import vue from '@vitejs/plugin-vue'
+
+/**
+ * https://vitejs.dev/config/
+ * @type {import('vite').UserConfig}
+ */
+
+
+export default {
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      port: 443,
+    }
+  }
+}
